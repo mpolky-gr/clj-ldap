a829330f7dc6da026eb7607619b760f3c56d1654
Matthew Polky <matthew.polky@guaranteedrate.com>
2017-06-08 15:15:42 -0500

    add my clojars repo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.pntblnk/clj-ldap "0.0.13"
+(defproject org.clojars.mpolky/clj-ldap "0.0.14"
   :description "Clojure ldap client."
-  :url "https://github.com/pauldorman/clj-ldap"
+  :url "https://github.com/mpolky-gr/clj-ldap"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.unboundid/unboundid-ldapsdk "3.2.0"]]
   :profiles {:test {:dependencies [[lein-clojars "0.9.1"]]}}
@@ -8,4 +8,6 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as Clojure"})
+            :comments "same as Clojure"}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :username :gpg}]])
